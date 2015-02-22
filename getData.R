6de61955eb0e8ec78fe28a197570e9a49344c74c
Bogdan Prisacari <bogdan.prisacari+github@gmail.com>
2015-02-22 23:37:17 +0100

    Added a script to fetch the original data

---
+++ getData.R
@@ -0,0 +1,2 @@
+download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip','getdata-projectfiles-UCI HAR Dataset.zip')
+unzip('getdata-projectfiles-UCI HAR Dataset.zip')
